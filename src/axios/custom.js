97c4e164fedb41be32326538ff1784e7d3e50aa5
yezarniko <yezarniko@protonmail.com>
2023-04-11 22:24:46 +0630

    Refector

--- src/axios/custom.js
+++ src/axios/custom.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+/**
+ * Axios instance for fetching CodeWars user data.
+ * @constant {AxiosInstance}
+ */
 export const codewarsUserDataFetch = axios.create({
   baseURL: "https://www.codewars.com/api/v1/users/",
   headers: {
