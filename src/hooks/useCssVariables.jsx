d6226407212dcb96538b74e6ca2d255f4a3fa2bc
yezarniko <yezarniko@protonmail.com>
2023-04-11 22:24:13 +0630

    Refector hook

--- src/hooks/useCssVariables.jsx
+++ src/hooks/useCssVariables.jsx
@@ -2,17 +2,61 @@ import { createContext, useContext } from "react";
 
 const CssVariablesContext = createContext(null);
 
+/**
+ * useCssVariables is a custom React Hook that provides access to CSS variables via context.
+ *
+ * @function
+ * @param {string} value - The name of the CSS variable to retrieve.
+ * @returns {*} - The value of the CSS variable.
+ *
+ * @example
+ * // Usage:
+ * // Import and use the useCssVariables hook in your component
+ * import { useCssVariables } from './useCssVariables';
+ *
+ * const MyComponent = () => {
+ *   const backgroundColor = useCssVariables('--background-color');
+ *   const textColor = useCssVariables('--text-color');
+ *
+ *   return (
+ *     <div style={{ backgroundColor, color: textColor }}>
+ *       {/* Render your component content /*}
+ *     </div>
+ *   );
+ * };
+ */
 export function useCssVariables(value) {
   return useContext(CssVariablesContext)(value);
 }
 
-export function CssVariablesProvider({ children }) {
-  function getStyleVar(name) {
-    return window
-      .getComputedStyle(document.documentElement)
-      .getPropertyValue(name);
-  }
+function getStyleVar(name) {
+  return window
+    .getComputedStyle(document.documentElement)
+    .getPropertyValue(name);
+}
 
+/**
+ * CssVariablesProvider is a higher-order component (HOC) that provides access to CSS variables via context.
+ *
+ * @function
+ * @param {ReactNode} props.children - The children components to be wrapped by the CssVariablesProvider.
+ * @returns {ReactNode} - The wrapped children components with access to CSS variables context.
+ *
+ * @example
+ * // Usage:
+ * // Import and use the CssVariablesProvider component to wrap your components
+ * import { CssVariablesProvider } from './CssVariablesProvider';
+ *
+ * const App = () => {
+ *   return (
+ *     <CssVariablesProvider>
+ *       <MyComponent />
+ *       <AnotherComponent />
+ *     </CssVariablesProvider>
+ *   );
+ * };
+ */
+export function CssVariablesProvider({ children }) {
   return (
     <CssVariablesContext.Provider value={getStyleVar}>
       {children}
